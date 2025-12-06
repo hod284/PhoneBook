106e9cec8973a350b1b581522743d7df68b3aa67
hod284 <61046278+hod284@users.noreply.github.com>
2025-12-07 02:38:36 +0900

    12
    qqq

--- src/main/java/com/example/AdressBook/EntityClass/PhoneNumberHistory.java
+++ src/main/java/com/example/AdressBook/EntityClass/PhoneNumberHistory.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
