33709f28d6d0bf5587ad7b407fedffd52b21728f
hod284 <61046278+hod284@users.noreply.github.com>
2025-12-07 11:17:57 +0900

    121
    12

--- src/test/java/com/example/AdressBook/AdressBookApplicationTests.java
+++ src/test/java/com/example/AdressBook/AdressBookApplicationTests.java
@@ -2,7 +2,8 @@ package com.example.AdressBook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.test.context.ActiveProfiles;
+@ActiveProfiles("test")
 @SpringBootTest
 class AdressBookApplicationTests {
 
