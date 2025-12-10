15567048b348cf8f683f944397922a03e092a0f8
hod284 <61046278+hod284@users.noreply.github.com>
2025-12-10 21:31:39 +0900

    12
    12

--- src/main/java/com/example/AdressBook/AdressBookApplication.java
+++ src/main/java/com/example/AdressBook/AdressBookApplication.java
@@ -1,12 +1,11 @@
 package com.example.AdressBook;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy
 @SpringBootApplication
-@MapperScan("com.example.AdressBook.Mapperclass")
 public class AdressBookApplication {
 
 	public static void main(String[] args) {
